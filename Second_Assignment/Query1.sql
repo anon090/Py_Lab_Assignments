401309a7853b454d45e93e0916db2de6a4fd821a
Anon <vishabhishek197@gmail.com>
2024-12-19 23:09:04 +0530

    Add files via upload

---
+++ Second_Assignment/Query1.sql
@@ -0,0 +1,3 @@
+-- Select all products from the 'product' table where 'prodname' is between 'a' and 'j' (inclusive)
+SELECT * FROM product 
+WHERE prodname BETWEEN 'a' AND 'j';
---
+++ Second_Assignment/Query2.sql
@@ -0,0 +1,3 @@
+-- Select all products from the 'product' table where the selling date is between '2024-08-12' and '2024-12-03'
+SELECT * FROM product
+WHERE selling_date BETWEEN '2024-08-12' AND '2024-12-03';
